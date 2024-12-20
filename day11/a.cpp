368ae7990a7fb02e8f9cd3be48d05204557166ca
Oscar Carballo Puebla <oscar.pecas.cp@gmail.com>
2024-12-20 19:49:09 +0100

    day11

---
+++ day11/a.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long 
+
+
+signed main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  freopen("input.txt","r",stdin);
+  map<int,int> M;
+  int x;
+  while(cin>>x){
+    M[x]++;
+  }
+  for(int i=0;i<25;i++){
+    cout<<i<<endl;
+    map<int,int> nM;
+    for(auto it = M.begin();it!=M.end();it++){
+      if(it->first==0){
+        nM[1] += it->second;
+      }
+      else if(to_string(it->first).length()%2==0){
+        int first_half = stoi(to_string(it->first).substr(0,to_string(it->first).length()/2));
+        int second_half = stoi(to_string(it->first).substr(to_string(it->first).length()/2));
+        nM[first_half] += it->second;
+        nM[second_half] += it->second;
+      }
+      else{
+        nM[it->first*2024] += it->second;
+      }
+    }
+    M = nM;
+  }
+  int sol = 0;
+  for(auto it = M.begin();it!=M.end();it++){
+    sol+=it->second;
+  }
+  cout<<sol;
+}
